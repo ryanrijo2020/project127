094a734fdfbea914a8312cf1761be2ce8acb1375
ryanrijo2020 <72882021+ryanrijo2020@users.noreply.github.com>
2021-04-30 21:42:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload() {
+
+}
+function setup() {
+    canvas = createCanvas(500, 500);
+    canvas.position(450,200);
+    video = createCapture(VIDEO);
+    video.size(500,500);
+    video.hide();
+}
+function draw() {
+
+}
